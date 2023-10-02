b33705bedf1fbf3b469a9b2fb22a80a83a01b4dd
b.kovalev <b.kovalev@g.nsu.ru>
2023-10-02 20:10:57 +0700

    first test works

--- lab_1/main.cpp
+++ lab_1/main.cpp
@@ -1,5 +1,5 @@
 #include "hash_table.hpp"
-#include <gtest/gtest.h>
+#include "googletest/googletest/include/gtest/gtest.h"
 
 TEST(hash_table_test, Constructors){
     hash_table first;
@@ -10,3 +10,5 @@ int main(int argc, char** argv){
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
+
+
