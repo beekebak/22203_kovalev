2ca2248f9c5a4999e71a0b1d6f0cd00209bdff13
beekebak <ribehadance@gmail.com>
2023-12-27 16:39:29 +0700

    made directories for the files

--- lab_2/include/choose_table.h
+++ lab_2/include/choose_table.h
@@ -1,7 +1,7 @@
 #include <map>
 #include <string>
 #include <fstream>
-#include "json/single_include/nlohmann/json.hpp"
+#include "../json/single_include/nlohmann/json.hpp"
 
 #ifndef CHOOSE_TABLE_H
 #define CHOOSE_TABLE_H
